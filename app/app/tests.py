b7f96ee28ad3c506254aa8db4dccc8c21484085a
irfanuddin89 <irfanuddin89@gmail.com>
2025-01-23 19:12:57 +0500

    database connection

---
+++ app/app/tests.py
@@ -0,0 +1,15 @@
+"""
+Sample Tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the Calc Module."""
+    
+    def test_add_numbers(self):
+        """test number add together"""
+        res = calc.add(4, 5)
+        self.assertEqual(res,9)
